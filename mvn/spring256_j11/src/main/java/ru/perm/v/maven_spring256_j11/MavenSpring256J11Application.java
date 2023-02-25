1f78741751eca1c947961c2d564ca5ee037b8283
Черепахин Василий <vasi.che@gmail.com>
2023-02-25 20:10:18 +0500

    add gitignore

---
+++ mvn/spring256_j11/src/main/java/ru/perm/v/maven_spring256_j11/MavenSpring256J11Application.java
@@ -0,0 +1,13 @@
+package ru.perm.v.maven_spring256_j11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenSpring256J11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenSpring256J11Application.class, args);
+	}
+
+}
---
+++ mvn/spring302_j11/src/test/java/ru/perm/v/maven_spring302_j11/MavenSpring302J11ApplicationTests.java
@@ -0,0 +1,15 @@
+package ru.perm.v.maven_spring302_j11;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+class MavenSpring302J11ApplicationTests {
+
+    @Test
+    void simple() {
+        assertEquals(1, 1);
+    }
+
+}
