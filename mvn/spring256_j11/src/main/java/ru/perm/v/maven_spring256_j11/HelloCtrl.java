5ede0941561ef25a5b9782f3b2f8bcc27f8f4301
Черепахин Василий <vasi.che@gmail.com>
2023-02-25 20:30:57 +0500

    optimize import

--- mvn/spring256_j11/src/main/java/ru/perm/v/maven_spring256_j11/HelloCtrl.java
+++ mvn/spring256_j11/src/main/java/ru/perm/v/maven_spring256_j11/HelloCtrl.java
@@ -4,9 +4,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 @RestController
 @RequestMapping("/hello")
 public class HelloCtrl {
