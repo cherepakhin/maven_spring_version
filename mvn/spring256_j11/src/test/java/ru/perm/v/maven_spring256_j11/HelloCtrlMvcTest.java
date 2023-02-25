63bf5c0196ce9341522bfc23000cb0ca1b9418a3
Черепахин Василий <vasi.che@gmail.com>
2023-02-25 22:07:09 +0500

    add HelloCtrlMvcTest.java

---
+++ mvn/spring256_j11/src/test/java/ru/perm/v/maven_spring256_j11/HelloCtrlMvcTest.java
@@ -0,0 +1,52 @@
+package ru.perm.v.maven_spring256_j11;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@ContextConfiguration(classes = {HelloCtrl.class})
+@WebMvcTest
+public class HelloCtrlMvcTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    private final String root = "/hello";
+
+    @Test
+    public void getHelloText() throws Exception {
+        MvcResult result = mockMvc.perform(get(root + "/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.ALL))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String ans = result.getResponse().getContentAsString();
+        assertEquals("{\"id\":0,\"name\":\"-\"}", ans);
+    }
+
+    @Test
+    public void getHelloJson() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        MvcResult result = mockMvc.perform(get(root + "/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.ALL))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String ans = result.getResponse().getContentAsString();
+        HelloDto receivedDto = mapper.readValue(ans, HelloDto.class);
+        HelloDto expectedDto = new HelloDto(0L,"-");
+        assertEquals(expectedDto, receivedDto);
+    }
+}
